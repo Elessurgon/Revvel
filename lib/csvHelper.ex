589fb374714d4d991970e72afe3bae4fdd4c5c08
Elessurgon <lancelotsycorax@gmail.com>
2024-10-27 19:32:18 +0530

    fml xlsx

--- lib/csvHelper.ex
+++ lib/csvHelper.ex
@@ -1,6 +1,4 @@
 defmodule Revvel.CsvHelper do
-  #c("lib/csvHelper.ex")
-  #Revvel.CsvHelper.read_csv("./test.csv")
   def read_csv(path) do
     File.stream!(path)
     |> CSV.decode(headers: true)
@@ -10,5 +8,18 @@ defmodule Revvel.CsvHelper do
       {_, stuff} ->
         IO.puts("not idea what happened decoding row: #{stuff}")
     end)
+  end
+
+
+  def xlsxReader(path, filterConditionFunction)  do
+    [ok: table_id] = Xlsxir.multi_extract(path)
+    #
+
+    table_id
+    |> Xlsxir.get_mda
+    |> Enum.filter(filterConditionFunction)
+    |> Enum.reduce([], fn {_key, value}, acc -> [value | acc] end)
+    |> Enum.reduce(0, fn map, acc -> acc + String.to_float(map[8]) end)
+    |> Float.round(2)
   end
 end
---
+++ lib/dataGather.ex
@@ -0,0 +1,9 @@
+defmodule Revvel.DataGather do
+
+
+  def start(url) do
+
+  end
+
+
+end
