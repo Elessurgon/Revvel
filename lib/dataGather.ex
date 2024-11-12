ded75df6274be70405e23a4ea7fb6f580b05e64a
Elessurgon <lancelotsycorax@gmail.com>
2024-11-12 20:55:50 +0530

    maybe change to a different headless browser

--- lib/dataGather.ex
+++ lib/dataGather.ex
@@ -4,9 +4,29 @@ defmodule Revvel.DataGather do
   def visitUrl(url) do
     Hound.start_session()
     navigate_to(url)
+    element = find_element(:xpath, "//*[contains(text(), 'Mutual Funds - Holdings statement')]")
+    IO.inspect(element)
+
+
+    success = click(element)
+    IO.inspect(success)
+    IO.inspect("clicked")
 
     :timer.sleep(1000)
-    File.write("testing.txt", page_source())
+    IO.inspect(outer_html(element))
+
+    Enum.each(1..100, fn _ ->
+      click(element)
+      IO.inspect("clicked")
+      :timer.sleep(1000)
+      # download_span = find_element(:xpath, "//*[contains(text(), 'Download')]")
+      # IO.inspect(download_span)
+      # visible_in_element?(download_span, "Download")
+    end)
+
+
+    File.write("testing.html", page_source())
+
     Hound.end_session()
     IO.puts "Worked!!!"
   end
